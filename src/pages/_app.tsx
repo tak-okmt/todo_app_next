8ffae18e6c2cddcea1f53d3aef5bb7b617231f93
tak-okmt <gcp632dsh@gmail.com>
2024-02-17 19:53:41 +0900

    firestoreに保存したデータから表示するように処理追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from "@chakra-ui/react";
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '@/lib/firebase';
 
 export type Todo = {
   id: number;
@@ -9,6 +11,8 @@ export type Todo = {
   detail?: string;
   status: "notStartYet" | "inProgress" | "completed";
   createdAt: Date;
+  uid: string;
+  user_uid: string;
 }
 
 type ContextType = {
@@ -20,6 +24,28 @@ export const TodoContext = createContext<ContextType>({} as ContextType)
 
 export default function App({ Component, pageProps }: AppProps) {
   const [todos, setTodos] = useState<Todo[]>([])
+
+  useEffect(() => {
+    const getTodoData = async () => {
+      let todosData: Todo[] = []
+      const querySnapShot = await getDocs(collection(db, "todos"))
+      querySnapShot.forEach((doc) => {
+        const data = doc.data()
+        const todoData = {
+          id: data.id,
+          title: data.title,
+          detail: data.detail,
+          status: data.status,
+          createdAt: data.createdAt.toDate(),
+          uid: doc.id,
+          user_uid: data.user_uid,
+        }
+        todosData.push(todoData)
+      })
+      setTodos(todosData)
+    }
+    getTodoData()
+  }, [])
 
   return (
     <ChakraProvider>
